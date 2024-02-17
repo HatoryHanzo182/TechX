688e5771aaa4371d5f93ad452cb7b1c7a4811e53
1lexaa <nice140@icloud.com>
2024-02-17 02:18:21 +0200

    new

--- next-techx/app/signin/page.jsx
+++ next-techx/app/signin/page.jsx
@@ -1,12 +1,7 @@
-import { redirect } from "next/navigation";
-
 import Signin from "@/components/authforms/Signin";
 import Nav from "@/components/Nav";
 
 export default async function Login() {
-  //   const session = await getServerSession();
-
-  //   if (session) redirect("/home");
   return (
     <>
       <Nav />
---
+++ next-techx/components/pages/ProfilePage.jsx
@@ -0,0 +1,148 @@
+import React from "react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogFooter,
+} from "@/components/ui/dialog";
+
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+export function ProfilePage() {
+  return (
+    <main>
+      <div className="pt-10"></div>
+      <Tabs
+        defaultValue="account"
+        className="w-full justify-center items-center  px-10 mt-24"
+      >
+        <TabsList className="grid w-full grid-cols-4">
+          <TabsTrigger value="account">Account</TabsTrigger>
+          <TabsTrigger value="password">Password</TabsTrigger>
+          <TabsTrigger value="orders">Orders</TabsTrigger>
+          <TabsTrigger value="settings">Settings</TabsTrigger>
+        </TabsList>
+        <TabsContent value="account">
+          <Card>
+            <CardHeader>
+              <CardTitle>Account</CardTitle>
+              <CardDescription>Your information</CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-2">
+              <div>
+                <img
+                  src="https://placekitten.com/200/200"
+                  alt="Profile"
+                  className="w-32 h-32 rounded-full hover:bg-black"
+                />
+              </div>
+              <div className="space-y-1">
+                <Label htmlFor="name">Name</Label>
+                <Input
+                  id="name"
+                  disabled
+                  className="cursor-pointer w-1/2"
+                  defaultValue="Name"
+                />
+              </div>
+              <div className="space-y-1">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="username"
+                  disabled
+                  className="cursor-pointer w-1/2"
+                  defaultValue="peduarte@gmail.com"
+                />
+              </div>
+            </CardContent>
+
+            <CardFooter>
+              <Dialog className="p-2">
+                <DialogTrigger asChild>
+                  <Button variant="outline">Edit Profile</Button>
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-[425px]">
+                  <DialogHeader>
+                    <DialogTitle>Edit profile</DialogTitle>
+                    <DialogDescription>
+                      Make changes to your profile here. Click save when you're
+                      done.
+                    </DialogDescription>
+                  </DialogHeader>
+                  <div className="grid gap-4 py-4">
+                    <div className="grid grid-cols-4 items-center gap-4">
+                      <Label htmlFor="name" className="text-right">
+                        Name
+                      </Label>
+                      <Input
+                        id="name"
+                        value="Pedro Duarte"
+                        className="col-span-3"
+                      />
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                      <Label htmlFor="username" className="text-right">
+                        Username
+                      </Label>
+                      <Input
+                        id="username"
+                        value="@peduarte"
+                        className="col-span-3"
+                      />
+                    </div>
+                  </div>
+                  <DialogFooter>
+                    <Button type="submit">Save changes</Button>
+                  </DialogFooter>
+                </DialogContent>
+              </Dialog>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+        <TabsContent value="password">
+          <Card>
+            <CardHeader>
+              <CardTitle>Password</CardTitle>
+              <CardDescription>
+                Change your password here. After saving, you'll be logged out.
+              </CardDescription>
+            </CardHeader>
+
+            <CardContent className="space-y-2">
+              <div className="space-y-1">
+                <Label htmlFor="current">Current password</Label>
+                <Input id="current" type="password" className="w-1/2" />
+              </div>
+              <div className="space-y-1">
+                <Label htmlFor="new">New password</Label>
+                <Input id="new" type="password" className="w-1/2" />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button>Save password</Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+        <TabsContent value="orders"></TabsContent>
+        <TabsContent value="settings"></TabsContent>
+      </Tabs>
+    </main>
+  );
+}
+
+export default ProfilePage;
