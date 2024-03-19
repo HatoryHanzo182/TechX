86bf8442ac0e89eb9efca6fac3252b4b17f9127b
1lexaa <nice140@icloud.com>
2024-03-19 21:15:02 +0200

    go

--- TechX-server/Configs/config_gmail.js
+++ TechX-server/Configs/config_gmail.js
@@ -1,60 +1,55 @@
 import dotenv from "dotenv";
-import nodemailer from 'nodemailer';
-import path from 'path';
-import fs from 'fs/promises';
-import { fileURLToPath } from 'url';
+import nodemailer from "nodemailer";
+import path from "path";
+import fs from "fs/promises";
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-dotenv.config({ path: path.resolve(__dirname, '../.env') });
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
-const _postcard_path = path.join(__dirname, '../Content/html/GmailVerefication.html');
+const _postcard_path = path.join(
+  __dirname,
+  "../Content/html/GmailVerefication.html"
+);
 
-async function ReadHTMLFile() 
-{
-  try 
-  {
-    const html = await fs.readFile(_postcard_path, 'utf-8');
+async function ReadHTMLFile() {
+  try {
+    const html = await fs.readFile(_postcard_path, "utf-8");
     return html;
-  } 
-  catch (error) 
-  {
-    console.error('Error reading HTML file:', error);
+  } catch (error) {
+    console.error("Error reading HTML file:", error);
     throw error;
   }
 }
 
-async function SendMail(postcard_content) 
-{
-  try 
-  {
-    const transporter = nodemailer.createTransport(
-    {
-      service: 'gmail',
-      auth: { user: process.env.GMAIL, pass: process.env.PASSWORD_GMAIL }
+async function SendMail(postcard_content) {
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: { user: process.env.GMAIL, pass: process.env.PASSWORD_GMAIL },
     });
 
-    const mail_options = 
-    {
-      from:  process.env.GMAIL,
-      to: 'scrinnpowerapp@gmail.com',
-      subject: 'Confirm your email on TechX',
-      html: postcard_content
+    const mail_options = {
+      from: process.env.GMAIL,
+      to: "nice140@icloud.com",
+      subject: "Confirm your email on TechX",
+      html: postcard_content,
     };
 
     const info = await transporter.sendMail(mail_options);
-  } 
-  catch (error) { console.error('Error sending email:', error); }
+  } catch (error) {
+    console.error("Error sending email:", error);
+  }
 }
 
-(async () => 
-{
-  try 
-  {
+(async () => {
+  try {
     const postcard = await ReadHTMLFile();
-    
+
     await SendMail(postcard);
-  } 
-  catch (error) { console.error('Error:', error); }
+  } catch (error) {
+    console.error("Error:", error);
+  }
 })();
